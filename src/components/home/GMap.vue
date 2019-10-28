8d21ed0a7a30266267bcf2b21a04fb28502101d5
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2019-10-27 22:04:05 -0300

    caputando geolocation e enviando p firebase

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -6,6 +6,8 @@
 
 <script>
 import firebase from 'firebase'
+import db from '@/firebase/init'
+
 export default {
  name: 'GMap',
  data(){
@@ -29,11 +31,31 @@ export default {
     }
   },
   mounted(){
+    // get current user
+    let user = firebase.auth().currentUser
+    console.log(user)
+
     // get user geolocation
     if(navigator.geolocation){
       navigator.geolocation.getCurrentPosition(pos => {
         this.lat = pos.coords.latitude
         this.lng = pos.coords.longitude
+
+        // find the user record and then update geoocords
+        db.collection('users').where('user_id', '==', user.uid).get()
+        .then(snapshot => {
+          snapshot.forEach((doc) => {
+            db.collection('users').doc(doc.id).update({
+              geolocation: {
+                lat: pos.coords.latitude,
+                lng: pos.coords.longitude
+              }
+            })
+          })
+        }).then(() => {
+          this.renderMap()
+        })
+
         this.renderMap()
       }, (err) => {
         console.log(err)
